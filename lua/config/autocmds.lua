55b56647b7f051a38474d3a722d5fe3214ba4986
star <star@starlancer.io>
2025-06-11 13:00:43 -0700

    fixed go tab spacing

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -11,3 +11,23 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+-- Format Go files on save AND set tab display
+vim.api.nvim_create_autocmd("FileType", {
+  desc = 'Go file settings',
+  pattern = "go",
+  group = vim.api.nvim_create_augroup('go-settings', { clear = true }),
+  callback = function()
+    -- Smaller tab display
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+
+    -- Format on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = 0, -- Only for this buffer
+      callback = function()
+        vim.lsp.buf.format({ async = false })
+      end,
+    })
+  end,
+})
