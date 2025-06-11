ac465176e358fbb61654317e0c3b0fe787f5f958
star <star@starlancer.io>
2025-06-11 08:25:50 -0700

    synced

--- lua/custom/plugins/nvim-hoverfloat.lua
+++ lua/custom/plugins/nvim-hoverfloat.lua
@@ -1,7 +1,8 @@
+local use_local = false -- Set to false to use GitHub version
+
 return {
-  {
-    -- Use GitHub version
-    "star-lance/nvim-hoverfloat",
+  use_local and {
+    dir = "/home/star/code/nvim-plugins/nvim-hoverfloat",
     build = "make install", -- Essential: builds the TUI binary
     config = function()
       require("hoverfloat").setup({
@@ -9,18 +10,38 @@ return {
           terminal_cmd = "kitty",
         },
         communication = {
-          update_delay = 50, -- Moved to correct location
+          update_delay = 50,
         },
-        auto_start = true,   -- Start automatically
+        auto_start = true,
       })
     end,
     -- Load for relevant filetypes including TSX
     ft = {
       "lua", "python", "javascript", "typescript",
-      "typescriptreact", "javascriptreact", -- TSX/JSX support
+      "typescriptreact", "javascriptreact",
       "rust", "go", "c", "cpp", "java"
     },
     -- Or use this instead of ft to load after LSP attaches
     -- event = "LspAttach",
+  } or {
+    "star-lance/nvim-hoverfloat",
+    build = "make install",
+    config = function()
+      require("hoverfloat").setup({
+        tui = {
+          terminal_cmd = "kitty",
+        },
+        communication = {
+          update_delay = 50,
+          debug = true, -- Enable debug output
+        },
+        auto_start = true,
+      })
+    end,
+    ft = {
+      "lua", "python", "javascript", "typescript",
+      "typescriptreact", "javascriptreact",
+      "rust", "go", "c", "cpp", "java"
+    },
   }
 }
